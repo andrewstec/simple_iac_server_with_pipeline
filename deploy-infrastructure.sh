23167d02f14c6b05acd8b35fe127b86179a8922c
Andrew Stec <andrewstec@gmail.com>
2020-10-25 12:26:55 -0700

    Updating deployment to utilize minimal resources for cost

---
+++ deploy-infrastructure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Curl to Digital Ocean API Deployment of Kubernetes Cluster
+
+curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${DIGITAL_OCEAN_API_KEY}" -d '{"name": "simple-nginx-cluster","region": "nyc1","version": "1.18","tags": ["cgi", "simple-iac-server-with-pipeline"],"node_pools": [{"size": "s-1vcpu-2gb","count": 1,"name": "nginx-pool","tags": ["nginx", "cgi"],"labels": {"service": "cgi", "priority": "high"}}]}' "https://api.digitalocean.com/v2/kubernetes/clusters" 
+
+
